c95a3206064db1082bd8d7475c32cb285190acd1
brazyon <117491562+brazyon@users.noreply.github.com>
2023-04-11 21:06:43 -0300

    Add files via upload

---
+++ principal.java
@@ -0,0 +1,24 @@
+package Principal;
+
+import java.net.*;
+import java.io.*;
+import javax.swing.*;
+
+public class principal {
+
+    public static void main(String args[]) {
+        try {
+            Socket servidor = new Socket("10.150.20.88", 4444);//endereco e porta
+            PrintWriter out = new PrintWriter(
+                    servidor.getOutputStream(), true);
+            String mensagem = "";
+            while (!mensagem.equals("fim")) {
+                mensagem = JOptionPane.
+                        showInputDialog("Digite a Mensagem ou fim para sair");
+                out.println(mensagem); //escreve a mensagem
+            }
+        } catch (IOException io) {
+            System.err.println("Problemas de IO");
+        }
+    }
+}
